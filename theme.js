95040ea7710f97b963ee35a8662de3830ed45a26
Nick <Npgb18@gmail.com>
2022-11-05 11:31:32 +0000

    design finished

--- theme.js
+++ theme.js
@@ -1,1 +1,24 @@
-document.body.dataset.theme = localStorage.theme;
+function toggleTheme(theme) {
+    const head = document.getElementsByTagName('head')[0];
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = `${theme}.css`;
+    head.appendChild(link);
+    document.body.className = theme;
+}
+
+const defaultTheme = localStorage.theme ? localStorage.theme : 'theme1';
+toggleTheme(defaultTheme);
+
+const themeBG = document.getElementById('theme-bg');
+
+const themeBtns = document.getElementsByClassName('theme-toggle');
+for (let i = 0; i < themeBtns.length; i++) {
+    themeBtns[i].addEventListener('click', function (e) {
+        const theme = e.target.id;
+        localStorage.theme = theme;
+        toggleTheme(theme);
+        themeBG.className = `theme-bg ${theme}`;
+    });
+}
